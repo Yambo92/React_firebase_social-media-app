93942cf6bad0122c01dbd73c0c1d343c37823a30
yambo92 <1810962886@qq.com>
2020-04-16 17:19:31 +0800

    'authentication'

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: "2020-04-16T04:46:14.664Z",
+            likeCount: 0,
+            commentCount: 0
+        }
+    ]
+}
